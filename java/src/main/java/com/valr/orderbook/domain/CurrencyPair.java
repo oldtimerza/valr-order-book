50f3db6a9c1fe3ee195074588dcb9c7cdcf11631
oldtimerza <bradleymorrisza@gmail.com>
2025-03-05 21:53:14 +0200

    Moved folders to simplify layout of project

--- java/src/main/java/com/valr/orderbook/domain/CurrencyPair.java
+++
@@ -1,7 +0,0 @@
-package com.valr.orderbook.domain;
-
-public enum CurrencyPair {
-    BTCUSD,
-    ETHUSD,
-    BTCZAR
-}
--- java/src/main/java/com/valr/orderbook/domain/InvalidCurrencyPairException.java
+++
@@ -1,7 +0,0 @@
-package com.valr.orderbook.domain;
-
-public class InvalidCurrencyPairException extends RuntimeException {
-    public InvalidCurrencyPairException(final String currencyPair) {
-        super("Invalid currency pair provided: " + currencyPair);
-    }
-}
